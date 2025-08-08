6fdd02c774a14524b0fa3ef4db5783fd5daf83cb
Sonu64 <sonusantu64@gmail.com>
2025-08-08 12:51:36 +0530

    Made Player a prefab and added rotation using + - keybinding

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,13 +3,45 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    Rigidbody rb;
+    [SerializeField] private float thrustStrength;
+    [SerializeField] private float rotationStrength;
 
     private void OnEnable() {
         thrust.Enable();
+        rotation.Enable();
     }
-    private void Update() {
+
+    private void Start() {
+        // current object's rigid body
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        Thrust();
+        Rotation();
+    }
+
+    private void Rotation() {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log("Rotation value: " + rotationInput);
+        if (rotationInput < 0) {
+            ApplyRotation(rotationStrength);
+        } // to the right 
+        else if (rotationInput > 0) {
+            ApplyRotation(-rotationStrength);
+        } // to the left
+
+    }
+
+    private void ApplyRotation(float rotationStrength) {
+        transform.Rotate(Vector3.forward * Time.fixedDeltaTime * rotationStrength);
+    }
+
+    private void Thrust() {
         if (thrust.IsPressed()) {
-            Debug.Log("Thrusting !");
+            rb.AddRelativeForce(thrustStrength * Time.fixedDeltaTime * Vector3.up);
         }
     }
 }
